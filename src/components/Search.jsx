9ec56d1891fbb359686de2ba4f56cedc92ef21b4
haven <haven.eldebaque@gmail.com>
2022-03-23 14:06:02 +1000

    initial style pushed to Update1_adding_searched_page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-
+import { useNavigate } from 'react-router-dom'; 
 
 function Search() {
 
 
     const [input, setInput] = useState('');
+    const navigate = useNavigate(); 
 
     // To load another page on inter, we need eventHandler:
     const submitHandler = (e) => {
         e.preventDefault(); 
+        navigate('/searched/'+input)
     }
 
 
